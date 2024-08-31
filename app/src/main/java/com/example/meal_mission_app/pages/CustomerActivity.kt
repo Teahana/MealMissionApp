39cb4612fbce1f6f1a9137aa925b7fbff7cd26eb
Adrian Alamu <alamujunior@gmail.com>
2024-08-31 14:05:57 +1200

    updated

--- app/src/main/java/com/example/meal_mission_app/pages/CustomerActivity.kt
+++ app/src/main/java/com/example/meal_mission_app/pages/CustomerActivity.kt
@@ -1,6 +1,9 @@
 package com.example.meal_mission_app.pages
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
@@ -13,6 +16,7 @@ import androidx.core.content.ContextCompat
 import com.example.meal_mission_app.R
 import com.example.meal_mission_app.objects.NetworkClient
 import com.example.meal_mission_app.objects.OfflineStorageService
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -23,6 +27,7 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.Polyline
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -43,8 +48,8 @@ class CustomerActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var runnable: Runnable
     private var isRunning = false
     private var currentPolyline: Polyline? = null
-    private var originMarker: Marker? = null // Keep track of the origin marker
-    private var destinationMarker: Marker? = null // Keep track of the destination marker
+    private var originMarker: Marker? = null //Keep track of the origin marker
+    private var destinationMarker: Marker? = null// Keep track of the destination marker
 
     private lateinit var apiKey: String
 
@@ -63,6 +68,11 @@ class CustomerActivity : AppCompatActivity(), OnMapReadyCallback {
         LatLng(-18.138677, 178.449131)
     )
     private var currentDummyIndex = 0
+
+    companion object {
+        private const val REQUEST_LOCATION_PERMISSIONS_CODE = 1001
+        private const val REQUEST_CHECK_SETTINGS = 1002
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -89,42 +99,6 @@ class CustomerActivity : AppCompatActivity(), OnMapReadyCallback {
             stopProcess()
         }
     }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        // Check for location permissions and fetch location if granted
-        checkLocationPermissionAndFetchLocation()
-    }
-
-    private fun checkLocationPermissionAndFetchLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // Permission granted, fetch location
-            fetchCustomerLocation { customerLocation ->
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(customerLocation, 15f))
-                if (destinationMarker == null) {
-                    destinationMarker = mMap.addMarker(MarkerOptions().position(customerLocation).title("Your Location"))
-                }
-            }
-        } else {
-            // Request permission
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == 1) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                // Permission granted, fetch location
-                checkLocationPermissionAndFetchLocation()
-            } else {
-                // Permission denied, show a message to the user
-                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
     private fun getApiKeyFromManifest(): String {
         try {
             val applicationInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
@@ -133,6 +107,119 @@ class CustomerActivity : AppCompatActivity(), OnMapReadyCallback {
         } catch (e: PackageManager.NameNotFoundException) {
             Log.e(TAG, "Failed to load API key from manifest", e)
             return ""
+        }
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // Check for location permissions and fetch location if granted
+        checkLocationPermissionAndSettings()
+    }
+
+    private fun checkLocationPermissionAndSettings() {
+        if (checkLocationPermission()) {
+            // Permission granted, check if location settings are enabled
+            checkLocationSettings()
+        } else {
+            // Request permission
+            requestLocationPermission()
+        }
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_LOCATION_PERMISSIONS_CODE
+        )
+    }
+
+    private fun checkLocationSettings() {
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            5000L // Interval in milliseconds
+        ).apply {
+            setMinUpdateIntervalMillis(5000L) // Fastest interval in milliseconds
+        }.build()
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+            .setAlwaysShow(true) // Show the dialog even if location settings are off
+
+        val settingsClient: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = settingsClient.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied. Fetch the location.
+            fetchCustomerLocation { customerLocation ->
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(customerLocation, 15f))
+                if (destinationMarker == null) {
+                    destinationMarker = mMap.addMarker(MarkerOptions().position(customerLocation).title("Your Location"))
+                }
+            }
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed by showing the user a dialog.
+                try {
+                    exception.startResolutionForResult(this@CustomerActivity, REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                    Toast.makeText(this, "Unable to resolve location settings.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                // Show a message to the user that they need to enable location services.
+                Toast.makeText(this, "Please enable location services to continue.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_LOCATION_PERMISSIONS_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission granted, check if location settings are enabled
+                    checkLocationSettings()
+                } else {
+                    // Permission denied, show a message to the user
+                    Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    // User agreed to make required location settings changes
+                    fetchCustomerLocation { customerLocation ->
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(customerLocation, 15f))
+                        if (destinationMarker == null) {
+                            destinationMarker = mMap.addMarker(MarkerOptions().position(customerLocation).title("Your Location"))
+                        }
+                    }
+                }
+                Activity.RESULT_CANCELED -> {
+                    // User chose not to make required location settings changes
+                    Toast.makeText(this, "Location services are necessary for this feature.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
@@ -167,8 +254,8 @@ class CustomerActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun fetchCustomerLocation(onLocationRetrieved: (LatLng) -> Unit) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-                println("Accuracy: " + location.accuracy)
                 if (location != null && location.accuracy <= 30) {
+                    println("Accuracy: " + location.accuracy)
                     val customerLatLng = LatLng(location.latitude, location.longitude)
                     // Update the destination marker
                     if (destinationMarker == null) {
